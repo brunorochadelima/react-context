fd80006a685ab1267d511d84c97565579f60e877
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-26 08:54:22 -0300

    Renomeando UsuarioContext

--- src/common/contexts/Usuario.js
+++ src/common/contexts/Usuario.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createContext } from "react";
 export const UsuarioContext = createContext();
+UsuarioContext.displayName = 'Usuário'
 
 export const UsuarioProvider = ({ children }) => {
   const [nome, setNome] = React.useState("");
